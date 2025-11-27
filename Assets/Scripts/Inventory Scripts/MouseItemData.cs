6fe74964a4558258115fa577d52dfb9f8e454a38
Maxence <169144059+maxenceb91@users.noreply.github.com>
2025-11-27 20:57:09 +0100

    Récupération de l'item quand on clique dessus avec la main vide

--- Assets/Scripts/Inventory Scripts/MouseItemData.cs
+++ Assets/Scripts/Inventory Scripts/MouseItemData.cs
@@ -6,10 +6,19 @@ public class MouseItemData : MonoBehaviour
 {
     public Image itemSprite;
     public TextMeshProUGUI ItemCount;
+    public InventorySlot AssignedInventorySlot;
 
     private void Awake()
     {
         itemSprite.color = Color.clear;
         ItemCount.text = "";
+    }
+
+    public void UpdateMouseSlot(InventorySlot invSlot)
+    {
+        AssignedInventorySlot.AssignItem(invSlot);
+        itemSprite.sprite = invSlot.ItemData.Icon;
+        ItemCount.text = invSlot.StackSize.ToString();
+        itemSprite.color = Color.white;
     }
 }
