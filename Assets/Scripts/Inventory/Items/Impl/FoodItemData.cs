770576adc358cdb0e6f4a312e9fc69b03b0b0aea
Maxence <169144059+maxenceb91@users.noreply.github.com>
2026-01-19 10:12:35 +0100

    Cooldown entre chaque item que l'on mange

--- Assets/Scripts/Inventory/Items/Impl/FoodItemData.cs
+++ Assets/Scripts/Inventory/Items/Impl/FoodItemData.cs
@@ -3,36 +3,37 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Inventory System/Food Item")]
 public class FoodItemData : InventoryItemData
 {
-    [Header("Utils References")]
     [SerializeField] private float foodValue;
-    [Header("Sound Effects")]
+    [SerializeField] private float eatDuration = 2f;
     public AudioClip eatSound;
     public float volume = 1f;
- 
+
     public override bool UseItem()
     {
-        if (eatSound == null)
+        PlayerHand playerHand = FindAnyObjectByType<PlayerHand>();
+        if (playerHand.IsEating)
         {
-            Debug.LogWarning("Eat sound is not assigned.");
-            return true;
+            return false;
         }
 
-        GameObject temp = new GameObject("EatSound");
-        AudioSource source = temp.AddComponent<AudioSource>();
 
-        source.clip = eatSound;
-        source.volume = volume;
-        source.spatialBlend = 0f;
-        source.Play();
-
-        Destroy(temp, eatSound.length);
+        playerHand.StartEating(eatDuration, this);
+        if (eatSound != null)
+        {
+            GameObject temp = new GameObject("EatSound");
+            AudioSource source = temp.AddComponent<AudioSource>();
+            source.clip = eatSound;
+            source.volume = volume;
+            source.Play();
+            Destroy(temp, eatSound.length);
+        }
 
         PlayerFoodLevel playerFoodLevel = FindAnyObjectByType<PlayerFoodLevel>();
         if (playerFoodLevel != null)
         {
             playerFoodLevel.EatFood(foodValue);
         }
-        
+
         return true;
     }
 }
