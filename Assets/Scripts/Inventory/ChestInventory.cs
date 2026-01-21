ea6b0d21abc2d2305857e2215a63e61759dd81fe
Maxence <169144059+maxenceb91@users.noreply.github.com>
2026-01-21 17:27:32 +0100

    Update du chest

--- Assets/Scripts/Inventory/ChestInventory.cs
+++ Assets/Scripts/Inventory/ChestInventory.cs
@@ -4,16 +4,50 @@ using UnityEngine.Events;
 public class ChestInventory : InventoryHolder, IInteractable
 {   
     [SerializeField] private PlayerInventoryHolder playerInventory;
+    [SerializeField] private AudioClip chestOpenSound;
+
+    private GameObject chestOpen;
+    private GameObject chestClosed;
+
     public UnityAction<IInteractable> OnInteractionComplete { get; set; }
+
+    protected override void Awake()
+    {
+        // Assumes:
+        // Child 0 = closed chest
+        // Child 1 = open chest
+        chestClosed = transform.GetChild(0).gameObject;
+        chestOpen = transform.GetChild(1).gameObject;
+
+        CloseChestUI();
+    }
+
     public void Interact(Interactor interactor, out bool interactSuccessful)
     {
         OnDynamicInventoryDisplayRequested?.Invoke(primaryInventorySystem);
-        PlayerInventoryHolder.OnPlayerBackPackDisplayRequested?.Invoke(playerInventory.SecondaryInventorySystem);
+        PlayerInventoryHolder.OnPlayerBackPackDisplayRequested
+            ?.Invoke(playerInventory.SecondaryInventorySystem);
+
         interactSuccessful = true;
+        OpenChestUI();
+    }
+
+    public void OpenChestUI()
+    {
+        AudioSource.PlayClipAtPoint(chestOpenSound, transform.position);
+        chestClosed.SetActive(false);
+        chestOpen.SetActive(true);
+    }
+
+    public void CloseChestUI()
+    {
+        chestClosed.SetActive(true);
+        chestOpen.SetActive(false);
     }
 
     public void EndInteraction()
     {
-        
+        // Assuming 'this' refers to the current instance of ChestInventory
+        this.CloseChestUI();
     }
 }
