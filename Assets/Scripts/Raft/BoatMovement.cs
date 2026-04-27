9e21f995d2b9c0528d40641e4080ad046334ce7c
QuentinDegougeEsiee-it <quentin.degouge@edu.esiee-it.fr>
2026-04-27 11:14:28 +0200

    add tout début d'item canne à peche + fix jump on the freaking raft

--- Assets/Scripts/Raft/BoatMovement.cs
+++ Assets/Scripts/Raft/BoatMovement.cs
@@ -5,10 +5,25 @@ public class BoatMovement : MonoBehaviour
     [Header("Réglages Navigation")]
     public float vitesse = 3f; // Vitesse de croisière
     public Vector3 direction = Vector3.forward; // Vers l'avant (Z)
+    private Rigidbody rb;
 
-    void Update()
+    void Start()
     {
-        // Le radeau avance en permanence dans la direction choisie
-        transform.Translate(direction * vitesse * Time.deltaTime, Space.World);
+        // 2. Aller chercher le composant Rigidbody attaché au bateau
+        rb = GetComponent<Rigidbody>();
+
+        // Sécurité au cas où tu aurais oublié d'ajouter un Rigidbody sur l'objet
+        if (rb == null)
+        {
+            Debug.LogError("Il manque un Rigidbody sur le bateau !");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (rb == null) return;
+
+        // Maintenant 'rb' est connu et le mouvement fonctionnera physiquement
+        rb.MovePosition(rb.position + direction * vitesse * Time.fixedDeltaTime);
     }
 }
